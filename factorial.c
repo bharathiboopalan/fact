a160177689fffe5f033dd1ee3ffd36863acf57b9
bharathiboopalan <bharathiboopalann@gmail.com>
2014-05-05 20:03:54 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-int n,i=1,f=1;//declaration and initialize i and f 
+int n,i=1,f=1;//declaration of n,i,f and initialize i and f 
 clrscr();
 printf("enter the number:");
 scanf("%d",&n);//get the number for factorial
